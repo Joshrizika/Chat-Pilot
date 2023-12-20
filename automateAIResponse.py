14b636ce40c1a383e82fc5f560fbfab6d9da7039
joshrizika <joshrizika@gmail.com>
2023-12-19 23:29:54 -0500

    adjusted functionality of code to work with GUI

--- automateAIResponse.py
+++ automateAIResponse.py
@@ -7,6 +7,7 @@ from openai import OpenAI
 import base64
 import requests
 import getpass
+from PIL import Image
 
 DB_PATH = f"/Users/{getpass.getuser()}/Library/Messages/chat.db" #path to chat.db file
 
@@ -60,7 +61,7 @@ def get_recent_messages(target_number, last_id_checked):
     for message in messages:
         message_id, text, is_image, filename = message
         if is_image:
-            processed_messages.append((message_id, text, bool(is_image), filename.replace('~', '/Users/joshrizika')))
+            processed_messages.append((message_id, text, bool(is_image), filename.replace('~', f'/Users/{getpass.getuser()}')))
         else:
             processed_messages.append((message_id, text, bool(is_image), filename))
     return processed_messages #return messages
@@ -68,15 +69,12 @@ def get_recent_messages(target_number, last_id_checked):
 # function: generate a response from GPT
 # parameters: incoming_message - string, conversation_history - list of messages, recipient_name - string, recipient_description - string
 # returns: response message
-def generate_response(incoming_message, conversation_history, recipient_name, recipient_description):
+def generate_response(incoming_message, conversation_history, user_name, recipient_name, recipient_description, gpt_model):
     client = OpenAI() #create OpenAI client
-    gpt_model = "gpt-4-1106-preview" #set GPT model
-    # gpt_model = "gpt-4" #set GPT model
-    # gpt_model = "gpt-3.5-turbo-1106" #set GPT model
     gpt_model_number = float(gpt_model.split("-")[1].split(".")[0]) #get model number
     messages = [{ #give GPT inital instruction prompt
         "role": "assistant", 
-        "content": f"You are an imaginative person named Josh responding to a text message from a person named {recipient_name}, this person's relationship to Josh can be described as {recipient_description}. Your responses should be indistinguishable from a real human's. Do not use phrases like 'how can I assist you today?' or anything similar that might imply assistance or service. If asked to perform a task, politely explain that you are currently unable to do so and provide a creative, believable reason. The reason can be whimsical or imaginative but should sound plausible and human-like. Always maintain the persona of a real person in the conversation."}]
+        "content": f"You are an imaginative person named {user_name} responding to a text message from a person named {recipient_name}, this person's relationship to {user_name} can be described as their {recipient_description}. Your responses should be indistinguishable from a real human's. Do not use phrases like 'how can I assist you today?' or anything similar that might imply assistance or service. If asked to perform a task, politely explain that you are currently unable to do so and provide a creative, believable reason. The reason can be whimsical or imaginative but should sound plausible and human-like. Always maintain the persona of a real person in the conversation."}]
     messages.extend(conversation_history)  #add previous conversation
 
     pattern = r'^.*Questioned “(.*?)”.*$' #define pattern to check for questioned text
@@ -119,13 +117,49 @@ def get_response_time(message, words_per_minute):
     return response_time #return response time
     # return 0 #return 0 for testing purposes
 
+# function: convert image using imagemagick
+# parameters: input_path - string, output_path - string
+# returns: output_path
+def convert_image_with_imagemagick(input_path, output_path):
+    subprocess.run(['magick', 'convert', input_path, output_path], check=True) #convert image using imagemagick
+    return output_path #return output path
+
+# function: convert image using Pillow
+# parameters: input_path - string, output_path - string, format - string
+# returns: output_path
+def convert_image(input_path, output_path, format='JPEG'):
+    try: #try to convert image using Pillow
+        with Image.open(input_path) as img: #open image
+            img.convert('RGB').save(output_path, format) #convert image
+        return output_path #return output path
+    except IOError: #if Pillow fails to convert image
+        return convert_image_with_imagemagick(input_path, output_path) #convert image using imagemagick
+
+# function: encode image to base64
+# parameters: filepath - string
+# returns: base64 image
+def encode_image_to_base64(filepath):
+    supported_formats = ['png', 'jpeg', 'gif', 'webp'] #define supported formats
+
+    file_ext = filepath.split('.')[-1].lower() #get file extension
+    if file_ext not in supported_formats: #if file extension is not supported
+        filepath = convert_image(filepath, 'temp_converted_image.jpeg') #convert image to jpeg
+
+    with open(filepath, "rb") as image_file: #open image file
+        base64_image = base64.b64encode(image_file.read()).decode('utf-8') #encode image to base64
+
+    if filepath == 'temp_converted_image.jpeg': #if image was converted
+        os.remove(filepath) #remove converted image
+
+    return base64_image #return base64 image
+
 # function: generate a description for the inputted image
 # parameters: message - string
 # returns: wait time in seconds
 def generate_image_description(filepath):
     api_key = os.getenv('OPENAI_API_KEY') #get OpenAI API key
-    with open(filepath, "rb") as image_file: #open image file
-        base64_image = base64.b64encode(image_file.read()).decode('utf-8') #encode image file as base64
+
+    base64_image = encode_image_to_base64(filepath) #encode image to base64
 
     headers = {
         "Content-Type": "application/json", #set content type to json
@@ -152,22 +186,116 @@ def generate_image_description(filepath):
     response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload) #generate response from GPT
 
     response_data = response.json() #get json response data
+    print("response_data: ", response_data)
     message_content = response_data['choices'][0]['message']['content'] #get message content from response data
-    formatted_message_content = "Respond to this description of an image as if you were viewing the image yourself.  " + message_content.replace('\n', ' ') #format message content
+    formatted_message_content = "Imagine you are directly looking at an image described as follows: '" + message_content.replace('\n', ' ') + "'. Please provide a brief and concise reaction to the image as if you were seeing it yourself, keeping your response short."
 
     return formatted_message_content #return formatted message content
                                          
 
-# function: have a conversation with AI
+# # function: have a conversation with AI using a name
+# # parameters: target_name - string, target_description - string
+# # returns: nothing
+# def converse_with_AI_name(target_name, user_name, target_description, words_per_minute, gpt_model):
+#     CONVERSATION_HISTORY = [] #create conversation history
+#     target_number = get_contact_number(target_name) #get target phone number
+#     print("listening for messages from {}".format(target_number))
+#     last_id_checked = get_last_message_id() #get id of last message
+
+#     while True: #loop forever
+#         check_interval = 5 #set check interval
+#         pattern = r'^(Loved|Liked|Disliked|Laughed at|Emphasized) “.*”$' #define pattern to check for reactions
+#         start_time = time.time() #get start time
+#         messages = []
+#         for message in get_recent_messages(target_number, last_id_checked): #for each message in recent messages
+#             if not re.match(pattern, message[1]): #if message is not a reaction
+#                 if message[2]: #if message is an image
+#                     messages.append((message[0], generate_image_description(message[3]), message[2], message[3])) #add image description to messages
+#                 else: #if message is not an image
+#                     messages.append(message) #add message to messages
+
+#         if len(messages) > 0: #if there are new messages
+#             concatenated_text = ' '.join([row[1] for row in messages[::-1]]) #concatenate messages
+#             print("concatenated_text: ", concatenated_text)
+
+#             print("generating ai response...")
+#             response_message = generate_response(concatenated_text, CONVERSATION_HISTORY, user_name, target_name, target_description, gpt_model) #generate response
+#             response_generation_time = time.time() - start_time #calculate response generation time
+#             print("response_message: ", response_message)
+#             print("response_generation_time: ", response_generation_time)
+
+#             response_time = get_response_time(response_message, words_per_minute) #get response time
+#             print("response_time: ", response_time)
+#             wait_time = response_time - response_generation_time #get wait time
+#             if wait_time < 0: #if response time is negative
+#                 wait_time = 0
+#             total_time_waited = wait_time + response_generation_time#set total time waited
+#             print("Sleeping for", wait_time, "seconds")
+#             time.sleep(wait_time) #sleep for response time
+
+#             print("checking for new messages...")
+#             start_time = time.time() #get start time
+#             new_messages = [] #create new messages list
+#             for message in get_recent_messages(target_number, last_id_checked): #for each message in recent messages
+#                 if not re.match(pattern, message[1]): #if message is not a reaction
+#                     print("message: ", message)
+#                     if message[2]: #if message is an image
+#                         new_messages.append((message[0], generate_image_description(message[3]), message[2], message[3]))
+#                     else:
+#                         new_messages.append(message)
+
+#             while len(new_messages) > len(messages): #while there are new messages
+#                 print("new message received")
+#                 messages = new_messages #update messages
+#                 concatenated_text = ' '.join([row[1] for row in messages[::-1]]) #concatenate messages
+#                 print("new concatenated_text: ", concatenated_text)
+
+#                 print("generating new ai response...")
+#                 response_message = generate_response(concatenated_text, CONVERSATION_HISTORY, user_name, target_name, target_description, gpt_model) #generate response
+#                 response_generation_time = time.time() - start_time #calculate response generation time
+
+#                 wait_time = get_response_time(response_message, words_per_minute) - response_generation_time #get response time
+#                 if wait_time < 0: #if response time is negative
+#                     wait_time = 0
+#                 remaining_wait_time = wait_time - total_time_waited #calculate wait time
+#                 if remaining_wait_time < 0: #if wait time is negative
+#                     remaining_wait_time = 0 #wait for 0 seconds
+#                 total_time_waited += remaining_wait_time + response_generation_time #update total time waited
+#                 print("Sleeping for", remaining_wait_time, "seconds")
+#                 time.sleep(remaining_wait_time) #sleep for response time
+
+#                 print("checking for new messages...")
+#                 start_time = time.time() #get start time
+#                 new_messages = [] #create new messages list
+#                 for message in get_recent_messages(target_number, last_id_checked): #for each message in recent messages
+#                     if not re.match(pattern, message[1]): #if message is not a reaction
+#                         print("message: ", message)
+#                         if message[2]: #if message is an image
+#                             new_messages.append((message[0], generate_image_description(message[3]), message[2], message[3])) #add image description to messages
+#                         else: #if message is not an image
+#                             new_messages.append(message) #add message to messages
+#             print("sending message responding to", concatenated_text)
+#             print("response_message: ", response_message)
+#             os.system(f'osascript sendMessage.applescript "{target_number}" "{response_message}"') #send response message         
+#             last_id_checked = messages[0][0] #update last id checked
+
+#         time.sleep(check_interval) #sleep for check interval
+
+def sleep_with_check(sleep_time, stop_flag):
+    elapsed_time = 0
+    while elapsed_time < sleep_time and not stop_flag.is_set():
+        time.sleep(min(1, sleep_time - elapsed_time))  # Sleep for 1 second or the remaining time
+        elapsed_time += 1
+
+# function: have a conversation with AI using a target name
 # parameters: target_name - string, target_description - string
 # returns: nothing
-def converse_with_AI(target_name, target_description, words_per_minute):
+def converse_with_AI(target_number, target_name, user_name, target_description, words_per_minute, gpt_model, stop_flag):
     CONVERSATION_HISTORY = [] #create conversation history
-    target_number = get_contact_number(target_name) #get target phone number
     print("listening for messages from {}".format(target_number))
     last_id_checked = get_last_message_id() #get id of last message
 
-    while True: #loop forever
+    while not stop_flag.is_set(): #loop forever
         check_interval = 5 #set check interval
         pattern = r'^(Loved|Liked|Disliked|Laughed at|Emphasized) “.*”$' #define pattern to check for reactions
         start_time = time.time() #get start time
@@ -184,7 +312,7 @@ def converse_with_AI(target_name, target_description, words_per_minute):
             print("concatenated_text: ", concatenated_text)
 
             print("generating ai response...")
-            response_message = generate_response(concatenated_text, CONVERSATION_HISTORY, target_name, target_description) #generate response
+            response_message = generate_response(concatenated_text, CONVERSATION_HISTORY, user_name, target_name, target_description, gpt_model) #generate response
             response_generation_time = time.time() - start_time #calculate response generation time
             print("response_message: ", response_message)
             print("response_generation_time: ", response_generation_time)
@@ -196,8 +324,10 @@ def converse_with_AI(target_name, target_description, words_per_minute):
                 wait_time = 0
             total_time_waited = wait_time + response_generation_time#set total time waited
             print("Sleeping for", wait_time, "seconds")
-            time.sleep(wait_time) #sleep for response time
+            sleep_with_check(wait_time, stop_flag) #sleep for response time if stop flag is not set
 
+            if stop_flag.is_set():
+                break
             print("checking for new messages...")
             start_time = time.time() #get start time
             new_messages = [] #create new messages list
@@ -216,9 +346,11 @@ def converse_with_AI(target_name, target_description, words_per_minute):
                 print("new concatenated_text: ", concatenated_text)
 
                 print("generating new ai response...")
-                response_message = generate_response(concatenated_text, CONVERSATION_HISTORY, target_name, target_description) #generate response
+                response_message = generate_response(concatenated_text, CONVERSATION_HISTORY, user_name, target_name, target_description, gpt_model) #generate response
                 response_generation_time = time.time() - start_time #calculate response generation time
 
+                if stop_flag.is_set():
+                    break
                 wait_time = get_response_time(response_message, words_per_minute) - response_generation_time #get response time
                 if wait_time < 0: #if response time is negative
                     wait_time = 0
@@ -227,8 +359,10 @@ def converse_with_AI(target_name, target_description, words_per_minute):
                     remaining_wait_time = 0 #wait for 0 seconds
                 total_time_waited += remaining_wait_time + response_generation_time #update total time waited
                 print("Sleeping for", remaining_wait_time, "seconds")
-                time.sleep(remaining_wait_time) #sleep for response time
+                sleep_with_check(remaining_wait_time, stop_flag) #sleep for response time if stop flag is not set
 
+                if stop_flag.is_set():
+                    break
                 print("checking for new messages...")
                 start_time = time.time() #get start time
                 new_messages = [] #create new messages list
@@ -244,7 +378,10 @@ def converse_with_AI(target_name, target_description, words_per_minute):
             os.system(f'osascript sendMessage.applescript "{target_number}" "{response_message}"') #send response message         
             last_id_checked = messages[0][0] #update last id checked
 
-        time.sleep(check_interval) #sleep for check interval
+        sleep_with_check(check_interval, stop_flag) #sleep for check interval if stop flag is not set
 
 if __name__ == "__main__":
-    converse_with_AI(target_name="Josh Rizika" , target_description="My self", words_per_minute=80)
+    gpt_model = "gpt-4-1106-preview" #set GPT model
+    # gpt_model = "gpt-4" #set GPT model
+    # gpt_model = "gpt-3.5-turbo-1106" #set GPT model
+    # converse_with_AI_name(target_name="Frank Minosh", user_name = "Josh Rizika" , target_description="grandpa", words_per_minute=2000, gpt_model=gpt_model)
